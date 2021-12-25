f1e6e8e9cc35cc1dd3d667753a55a89dfd54323e
hvakanksha <88264876+hvakanksha@users.noreply.github.com>
2021-12-25 17:03:43 +0530

    Add files via upload

---
+++ TimeSEariesUNP.R
@@ -0,0 +1,346 @@
+install.packages("readr")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/readr_2.1.1.zip'
+Content type 'application/zip' length 1798159 bytes (1.7 MB)
+downloaded 1.7 MB
+
+package ‘readr’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("ggplot2")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/ggplot2_3.3.5.zip'
+Content type 'application/zip' length 4130508 bytes (3.9 MB)
+downloaded 3.9 MB
+
+package ‘ggplot2’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("TSA")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/TSA_1.3.zip'
+Content type 'application/zip' length 535258 bytes (522 KB)
+downloaded 522 KB
+
+package ‘TSA’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("tseries")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/tseries_0.10-49.zip'
+Content type 'application/zip' length 410313 bytes (400 KB)
+downloaded 400 KB
+
+package ‘tseries’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("fUnitRoots")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/fUnitRoots_3042.79.zip'
+Content type 'application/zip' length 672418 bytes (656 KB)
+downloaded 656 KB
+
+package ‘fUnitRoots’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("forecast")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/forecast_8.15.zip'
+Content type 'application/zip' length 2342224 bytes (2.2 MB)
+downloaded 2.2 MB
+
+package ‘forecast’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("lmtest")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/lmtest_0.9-39.zip'
+Content type 'application/zip' length 414853 bytes (405 KB)
+downloaded 405 KB
+
+package ‘lmtest’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("gtools")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/gtools_3.9.2.zip'
+Content type 'application/zip' length 367054 bytes (358 KB)
+downloaded 358 KB
+
+package ‘gtools’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> install.packages("MLmetrics")
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+  
+  https://cran.rstudio.com/bin/windows/Rtools/
+  Installing package into ‘C:/Users/akank/OneDrive/Documents/R/win-library/4.1’
+(as ‘lib’ is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/4.1/MLmetrics_1.1.1.zip'
+Content type 'application/zip' length 75848 bytes (74 KB)
+downloaded 74 KB
+
+package ‘MLmetrics’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\akank\AppData\Local\Temp\RtmpM11nHJ\downloaded_packages
+> library(readr)
+> library(ggplot2)
+> library(TSA)
+
+Attaching package: ‘TSA’
+
+The following object is masked from ‘package:readr’:
+  
+  spec
+
+The following objects are masked from ‘package:stats’:
+  
+  acf, arima
+
+The following object is masked from ‘package:utils’:
+  
+  tar
+
+> library(tseries)
+Registered S3 method overwritten by 'quantmod':
+  method            from
+as.zoo.data.frame zoo 
+
+‘tseries’ version: 0.10-49
+
+‘tseries’ is a package for time series analysis and computational
+finance.
+
+See ‘library(help="tseries")’ for details.
+
+> library(fUnitRoots)
+Loading required package: timeDate
+
+Attaching package: ‘timeDate’
+
+The following objects are masked from ‘package:TSA’:
+  
+  kurtosis, skewness
+
+Loading required package: timeSeries
+Loading required package: fBasics
+> library(forecast)
+Registered S3 methods overwritten by 'forecast':
+  method       from
+fitted.Arima TSA 
+plot.Arima   TSA 
+> library(lmtest)
+Loading required package: zoo
+
+Attaching package: ‘zoo’
+
+The following object is masked from ‘package:timeSeries’:
+  
+  time<-
+  
+  The following objects are masked from ‘package:base’:
+  
+  as.Date, as.Date.numeric
+
+> library(gtools)
+> library(MLmetrics)
+
+Attaching package: ‘MLmetrics’
+
+The following object is masked from ‘package:base’:
+  
+  Recall
+
+> df <- read_csv("C:\Users\akank\OneDrive\Desktop\shampoo.txt")
+Error: '\U' used without hex digits in character string starting ""C:\U"
+> df <- read_csv("C:/Users/akank/OneDrive/Desktop/shampoo.txt")
+Rows: 36 Columns: 2                                                                
+-- Column specification -------------------------------------------------------------
+Delimiter: ","
+chr (1): Month
+dbl (1): Sales
+
+i Use `spec()` to retrieve the full column specification for this data.
+i Specify the column types or set `show_col_types = FALSE` to quiet this message.
+> plot(df$Sales,type="o")
+> #Converting into Time series 
+> dfc <-ts(df[,2],start=2000,frequency=12)
+> dfc
+       Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+2000 266.0 145.9 183.1 119.3 180.3 168.5 231.8 224.5 192.8 122.9 336.5 185.9
+2001 194.3 149.5 210.1 273.3 191.4 287.0 226.0 303.6 289.9 421.6 264.5 342.3
+2002 339.7 440.4 315.9 439.3 401.3 437.4 575.5 407.6 682.0 475.3 581.3 646.9
+> plot(dfc,type="o")
+> plot(decompose(dfc))
+> tsdisplay(dfc,main="ACF-PACF-OBSERVERED PLOTS")
+> ggseasonplot(dfc,year.labels = TRUE)
+> points(y=dfc,x=time(dfc), pch=as.vector(season(dfc)))
+> #Auto-Correlation and Partial Auto Correlation Function(ACF &  PACF)
+Warning messages:
+1: In doTryCatch(return(expr), name, parentenv, handler) :
+  invalid graphics state
+2: In doTryCatch(return(expr), name, parentenv, handler) :
+  invalid graphics state
+> acf(dfc, lag.max = 36) 
+> pacf(dfc, lag.max = 36)
+> 
+> #Train and Test
+> traindf<-ts(dfc[1:24],start=2000,frequency = 12)
+> traindf  #67%
+       Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+2000 266.0 145.9 183.1 119.3 180.3 168.5 231.8 224.5 192.8 122.9 336.5 185.9
+2001 194.3 149.5 210.1 273.3 191.4 287.0 226.0 303.6 289.9 421.6 264.5 342.3
+> acf(traindf)
+> pacf(traindf)
+> tsdisplay(traindf,main = "ACF-PACF-OBSERVERED PLOTS OF TRAIN")
+> testdf<-ts(dfc[25:36],start=c(2002,01),frequency = 12)
+> testdf   #33%
+       Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+2002 339.7 440.4 315.9 439.3 401.3 437.4 575.5 407.6 682.0 475.3 581.3 646.9
+> adf.test(traindf)
+
+	Augmented Dickey-Fuller Test
+
+data:  traindf
+Dickey-Fuller = -3.0324, Lag order = 2, p-value = 0.1791
+alternative hypothesis: stationary
+
+> adf.test(testdf)
+
+	Augmented Dickey-Fuller Test
+
+data:  testdf
+Dickey-Fuller = -1.4513, Lag order = 2, p-value = 0.7814
+alternative hypothesis: stationary
+
+> 
+> #first order differencing  d=0
+> dif=diff(traindf)
+> dif
+        Jan    Feb    Mar    Apr    May    Jun    Jul    Aug    Sep    Oct    Nov
+2000        -120.1   37.2  -63.8   61.0  -11.8   63.3   -7.3  -31.7  -69.9  213.6
+2001    8.4  -44.8   60.6   63.2  -81.9   95.6  -61.0   77.6  -13.7  131.7 -157.1
+        Dec
+2000 -150.6
+2001   77.8
+> acf(dif)
+> pacf(dif)
+> adf.test(dif)
+
+	Augmented Dickey-Fuller Test
+
+data:  dif
+Dickey-Fuller = -4.8787, Lag order = 2, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(dif) : p-value smaller than printed p-value
+> tsdisplay(dif,main="ACF-PACF-OBSERVED PLOTS OF DIFFERENCING")
+> #second order differencing d=1
+> dif2=diff(dif)
+> dif2
+        Jan    Feb    Mar    Apr    May    Jun    Jul    Aug    Sep    Oct    Nov
+2000                157.3 -101.0  124.8  -72.8   75.1  -70.6  -24.4  -38.2  283.5
+2001  159.0  -53.2  105.4    2.6 -145.1  177.5 -156.6  138.6  -91.3  145.4 -288.8
+        Dec
+2000 -364.2
+2001  234.9
+> autoplot(dif2)
+> acf(dif2)
+> pacf(dif2)
+> adf.test(dif2)
+
+	Augmented Dickey-Fuller Test
+
+data:  dif2
+Dickey-Fuller = -5.0963, Lag order = 2, p-value = 0.01
+alternative hypothesis: stationary
+
+Warning message:
+In adf.test(dif2) : p-value smaller than printed p-value
+> tsdisplay(dif2)
+> #Arima
+> fit=Arima(traindf,c(1,0,1))   
+> fit
+Series: traindf 
+ARIMA(1,0,1) with non-zero mean 
+
+Coefficients:
+         ar1      ma1      mean
+      0.8893  -0.6412  239.4946
+s.e.  0.1476   0.2007   36.6293
+
+sigma^2 estimated as 5035:  log likelihood=-134.94
+AIC=277.89   AICc=279.99   BIC=282.6
+> auto.arima(traindf)
+Series: traindf 
+ARIMA(1,1,0) 
+
+Coefficients:
+          ar1
+      -0.6761
+s.e.   0.1547
+
+sigma^2 estimated as 4664:  log likelihood=-129.58
+AIC=263.15   AICc=263.75   BIC=265.43
+Warning message:
+The chosen seasonal unit root test encountered an error when testing for the first difference.
+From stl(): series is not periodic or has less than two periods
+0 seasonal differences will be used. Consider using a different unit root test. 
+> 
+> #Prediction
+> pre<-predict(fit,12)
+> pree<-round(pre$pred,1)
+> pree
+       Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+2002 293.1 287.2 281.9 277.2 273.0 269.3 266.0 263.1 260.5 258.2 256.1 254.2
+> 
+> length(pre$pred)
+[1] 12
+> 
+> #Mean Absolute Percentage Error
+> map<-round(MAPE(traindf[13:24],pree),3)
+> map
+[1] 0.24
+> 
